973e99f081815e283cbe229c24dcc5a1fa7bbc4d
szkozielek <61829857+szkozielek@users.noreply.github.com>
2025-03-04 22:59:59 +0100

    Initial commit

---
+++ src/main/java/pl/szkozielek/sales_funnel/SalesFunnelApplication.java
@@ -0,0 +1,13 @@
+package pl.szkozielek.sales_funnel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesFunnelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesFunnelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/szkozielek/sales_funnel/SalesFunnelApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.szkozielek.sales_funnel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesFunnelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
