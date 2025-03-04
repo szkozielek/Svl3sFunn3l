23b1fa20c0e84a560f24049885b4d3d34a679274
szkozielek <61829857+szkozielek@users.noreply.github.com>
2025-03-05 00:22:32 +0100

    ➕ feat(Login): Dodanie obsługi tailwindcss oraz stworzenie prostego formularza logowania

---
+++ src/main/java/pl/szkozielek/sales_funnel/controllers/LoginController.java
@@ -0,0 +1,14 @@
+package pl.szkozielek.sales_funnel.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String index(){
+        return "pages/login";
+    }
+}
